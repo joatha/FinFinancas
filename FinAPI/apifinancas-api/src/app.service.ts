88b16e29cc0b305c490b2ca4629e6b6dcbb4eeaf
joatha <joathaandrade2@gmail.com>
2021-02-25 09:20:08 -0300

    Criando API

---
+++ FinAPI/apifinancas-api/src/app.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class AppService {
+  getHello(): string {
+    return 'Hello World!';
+  }
+
+  getObject(): any{
+    let objeto ={
+      id:1,
+      nome:"joatha"
+  }
+  return objeto
+}
+
+}
